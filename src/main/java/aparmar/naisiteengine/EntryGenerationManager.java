46136de65857b69bac23e171ddf905c577ab9582
aparmar2000 <aparmar2000@gmail.com>
2023-12-18 19:51:48 -0700

    Rewrite of entry generation thread

--- src/main/java/aparmar/naisiteengine/EntryGenerationManager.java
+++ src/main/java/aparmar/naisiteengine/EntryGenerationManager.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
@@ -28,7 +29,9 @@ import aparmar.naisiteengine.entry.EntryData;
 import aparmar.naisiteengine.entry.EntryManager;
 import aparmar.naisiteengine.entry.EntryTextGenerationConfig;
 import aparmar.naisiteengine.entry.EntryType;
+import aparmar.naisiteengine.entry.TagGroupData;
 import aparmar.naisiteengine.entry.TagGroupData.TagEntry;
+import aparmar.naisiteengine.utils.NaiSiteEngineUtils;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -45,65 +48,86 @@ public class EntryGenerationManager implements Runnable {
 
 	@Override
 	public void run() {
-		ENTRY_GEN_THREAD_LOGGER.info("Entry generation thread started.");
-		stopFlag.set(false);
-		final int targetCacheSize = userConfig.getGenerationConfig().getTargetCacheSize();
-		final Random rng = new Random();
-		
-		while (!stopFlag.get()) {
-			Map<String, Integer> unreadCountByType = 
-					entryManager.getUnreadGeneratedEntryCountByType();
-			int totalUnread = unreadCountByType.values().stream()
-					.mapToInt(Integer::intValue)
-					.sum();
-			while (totalUnread < userConfig.getGenerationConfig().getTargetCacheSize()) {
-				ENTRY_GEN_THREAD_LOGGER.info("Entry cache at "+totalUnread+"/"+targetCacheSize+"; Generating more...");
-				
-				String leastCachedType = unreadCountByType.entrySet().stream()
-					.min(Comparator.comparingInt(Map.Entry::getValue))
-					.get().getKey();
-				
-				try {
-					EntryData newArticle = generateEntryOfType(leastCachedType, rng.nextInt(5)==0);
-					if (newArticle == null) {
-						ENTRY_GEN_THREAD_LOGGER.info("Entry generation failed.");
-					} else {
-						entryManager.addGeneratedEntry(newArticle);
-						ENTRY_GEN_THREAD_LOGGER.info("Entry generated, saving...");
-						entryManager.saveGeneratedEntriesToFileSystem();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				
-				
-				unreadCountByType = 
+		try {
+			ENTRY_GEN_THREAD_LOGGER.info("Entry generation thread started.");
+			stopFlag.set(false);
+			final int targetCacheSize = userConfig.getGenerationConfig().getTargetCacheSize();
+			final Random rng = new Random();
+			
+			while (!stopFlag.get()) {
+				Map<String, Integer> unreadCountByType = 
 						entryManager.getUnreadGeneratedEntryCountByType();
-				totalUnread = unreadCountByType.values().stream()
+				int totalUnread = unreadCountByType.values().stream()
 						.mapToInt(Integer::intValue)
 						.sum();
+				while (totalUnread < userConfig.getGenerationConfig().getTargetCacheSize()) {
+					ENTRY_GEN_THREAD_LOGGER.info("Entry cache at "+totalUnread+"/"+targetCacheSize+"; Generating more...");
+					
+					String leastCachedType = unreadCountByType.entrySet().stream()
+						.min(Comparator.comparingInt(Map.Entry::getValue))
+						.get().getKey();
+					
+					try {
+						EntryData newArticle = generateEntryOfType(leastCachedType, rng.nextInt(5)==0);
+						if (newArticle == null) {
+							ENTRY_GEN_THREAD_LOGGER.info("Entry generation failed.");
+						} else {
+							entryManager.addGeneratedEntry(newArticle);
+							ENTRY_GEN_THREAD_LOGGER.info("Entry generated, saving...");
+							entryManager.saveGeneratedEntriesToFileSystem();
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					
+					
+					unreadCountByType = 
+							entryManager.getUnreadGeneratedEntryCountByType();
+					totalUnread = unreadCountByType.values().stream()
+							.mapToInt(Integer::intValue)
+							.sum();
+				}
+				
+				try { Thread.sleep(500); } catch (InterruptedException e) { }
 			}
 			
-			try { Thread.sleep(500); } catch (InterruptedException e) { }
+			ENTRY_GEN_THREAD_LOGGER.info("Entry generation thread stopped.");
+		} catch (Exception e) {
+			ENTRY_GEN_THREAD_LOGGER.error(e.getLocalizedMessage(), e);
 		}
-		
-		ENTRY_GEN_THREAD_LOGGER.info("Entry generation thread stopped.");
 	}
 	
 	private static final int MAX_GENERATION_RETRIES = 2;
 	private EntryData generateEntryOfType(String entryTypeName, boolean useGenerated) throws IOException {
 		long seed = System.currentTimeMillis();
+		Random rng = new Random(seed);
 		
 		TextGenModel textModel = userConfig.getGenerationConfig().getModel();
 		EntryType entryType = siteConfigManager.getEntryTypeManager().getEntryTypeByName(entryTypeName);
 		
+		LinkedList<TagEntry> selectedTags = new LinkedList<>();
+		for (TagGroupData tagGroup : siteConfigManager.getTagGroupManager().getTagGroups()) {
+			TagEntry[] tagOptions = tagGroup.getTagEntries();
+			NaiSiteEngineUtils.shuffleArray(tagOptions, rng);
+			for (int i=0;i<rng.nextInt(tagGroup.getMaxSelectedValues())+1;i++) {
+				selectedTags.add(tagOptions[i]);
+			}
+		}
+		ENTRY_GEN_THREAD_LOGGER.info(
+				"\tSelected tags '"+
+				selectedTags.stream()
+					.map(TagEntry::getName)
+					.collect(Collectors.joining("','"))+
+				"' for generation.");
+		
 		EntryData newEntryData = siteConfigManager.createEntry(entryType);
+		newEntryData.setTags(selectedTags.stream().map(TagEntry::getName).toArray(String[]::new));
 		for (int i=0;i<entryType.getTextGenerationConfigs().length;i++) {
 			int retries = 0;
 			while (true) {
 				retries++;
 				String generatedText = 
-						generateForTextGenerationConfig(textModel, new TagEntry[0], seed, useGenerated, entryType, i);
+						generateForTextGenerationConfig(textModel, selectedTags.toArray(new TagEntry[0]), seed, useGenerated, entryType, i);
 				boolean successfullyPopulated = false;
 				if (generatedText != null) {
 					successfullyPopulated = entryType.populateFieldMapFromTextGen(newEntryData, i, generatedText);
@@ -133,31 +157,40 @@ public class EntryGenerationManager implements Runnable {
 		LogitBias endingBias = LogitBias.builder()
 				.sequence(encodedEndingSequence)
 				.bias(0)
-				.ensureSequenceFinishes(true)
+				.ensureSequenceFinishes(false)
 				.unbiasOnceGenerated(true)
 				.build();
+		
+		TokenizedChunk tagChunk = new TokenizedChunk(
+				tokenizer, 
+				TagEntry.toTextGenerationContextForm(tags)+"\n");
 		TokenizedChunk generatedChunk = new TokenizedChunk(
 				tokenizer, 
-				TagEntry.toTextGenerationContextForm(tags));
+				"");
 		
 		int generations = 0;
+		int generatedLength = 0;
 		while (!textGenerationConfig.getContentMatcher(generatedChunk.getTextChunk()).find()) {
 			TokenizedChunk exampleContextChunk = contextManager.buildContext(
 					tags, textGenerationConfigIndex,
 					baseParameters.getMaxLength()-generatedChunk.tokenLength()-5, 
 					useGenerated, seed);
-			TokenizedChunk fullContext = TokenizedChunk.mergeTokenizedChunks(tokenizer, exampleContextChunk, generatedChunk);
-
+			TokenizedChunk fullContext = TokenizedChunk.mergeTokenizedChunks(tokenizer, exampleContextChunk, tagChunk, generatedChunk);
+			
 			TextGenerationParameters parameters = baseParameters.toBuilder().build();
-			if (generatedChunk.textLength() < textGenerationConfig.getTargetMinLength()) {
-				double proportionSatisfied = generatedChunk.textLength()/textGenerationConfig.getTargetMinLength();
-				endingBias.setBias((1-proportionSatisfied) * -1.5);
-			} else if (generatedChunk.textLength() > textGenerationConfig.getTargetMaxLength()*0.75) {
+			parameters.setStopSequences(Optional.ofNullable(parameters.getStopSequences()).orElse(new LinkedList<>()));
+			parameters.getStopSequences().add(encodedEndingSequence);
+			endingBias.setEnsureSequenceFinishes(false);
+			if (generatedLength < textGenerationConfig.getTargetMinLength()) {
+				double proportionSatisfied = generatedLength/textGenerationConfig.getTargetMinLength();
+				endingBias.setBias((1-proportionSatisfied) * -0.05);
+			} else if (generatedLength > textGenerationConfig.getTargetMaxLength()*0.75) {
+				endingBias.setEnsureSequenceFinishes(true);
 				double rampLower = textGenerationConfig.getTargetMaxLength()*0.75;
 				double rampUpper = textGenerationConfig.getTargetMaxLength()*0.9;
 				double rampRange = rampUpper - rampLower;
 				
-				endingBias.setBias((generatedChunk.textLength()-rampLower)/rampRange * 1.2);
+				endingBias.setBias((generatedLength-rampLower)/rampRange * 1.2);
 			}
 			parameters.setLogitBiases(Optional.ofNullable(parameters.getLogitBiases()).orElse(new LinkedList<>()));
 			parameters.getLogitBiases().add(endingBias);
@@ -182,18 +215,19 @@ public class EntryGenerationManager implements Runnable {
 			}
 			
 			generatedChunk.appendTokenizedChunk(latestGeneration.getOutput());
+			generatedLength = generatedChunk.tokenLength();
 			
-			if (generatedChunk.textLength() > textGenerationConfig.getTargetMaxLength()) {
-				ENTRY_GEN_THREAD_LOGGER.info("\tEntry-in-progress too long ("+generatedChunk.textLength()+"/"+textGenerationConfig.getTargetMinLength()+"), discarded.");
+			if (generatedLength > textGenerationConfig.getTargetMaxLength()) {
+				ENTRY_GEN_THREAD_LOGGER.info("\tEntry-in-progress too long ("+generatedLength+"/"+textGenerationConfig.getTargetMaxLength()+"), discarded.");
 				return null;
 			}
 			
 			generations++;
-			ENTRY_GEN_THREAD_LOGGER.info("\tGenerating '"+entryType.getName()+"' entry... (step "+generations+", length "+String.format("%,d", generatedChunk.getTokens().length)+")");
+			ENTRY_GEN_THREAD_LOGGER.info("\tGenerating '"+entryType.getName()+"' entry... (step "+generations+", length "+String.format("%,d", generatedLength)+")");
 		}
 		
-		if (generatedChunk.textLength() < textGenerationConfig.getTargetMinLength()) {
-			ENTRY_GEN_THREAD_LOGGER.info("\tGenerated entry too short ("+generatedChunk.textLength()+"/"+textGenerationConfig.getTargetMinLength()+"), discarded.");
+		if (generatedLength < textGenerationConfig.getTargetMinLength()) {
+			ENTRY_GEN_THREAD_LOGGER.info("\tGenerated entry too short ("+generatedLength+"/"+textGenerationConfig.getTargetMinLength()+"), discarded.");
 			return null;
 		}
 		
