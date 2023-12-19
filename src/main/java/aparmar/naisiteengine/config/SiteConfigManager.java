35150b2cb991b87dd1c2d55dbde1e228744aed31
aparmar2000 <aparmar2000@gmail.com>
2023-12-18 19:46:23 -0700

    Added usage of default config files

--- src/main/java/aparmar/naisiteengine/config/SiteConfigManager.java
+++ src/main/java/aparmar/naisiteengine/config/SiteConfigManager.java
@@ -1,8 +1,15 @@
 package aparmar.naisiteengine.config;
 
+import static aparmar.naisiteengine.utils.NaiSiteEngineConstants.MAIN_THREAD_LOGGER;
+
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDateTime;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import aparmar.naisiteengine.entry.EntryData;
 import aparmar.naisiteengine.entry.EntryType;
@@ -11,12 +18,28 @@ import aparmar.naisiteengine.entry.TagGroupManager;
 import lombok.Getter;
 
 public class SiteConfigManager {
+	private static final String[] defaultConfigFiles = new String[] {
+			"entry-article.yaml",
+			"tag-group-category.yaml"
+	};
+	
 	@Getter
 	private final EntryTypeManager entryTypeManager;
 	@Getter
 	private final TagGroupManager tagGroupManager;
 	
 	public SiteConfigManager(File rootConfigDirectory, UserConfiguration config) throws IOException {
+		if (!rootConfigDirectory.isDirectory()) {
+			MAIN_THREAD_LOGGER.info("Website config folder not found, writing default configs...");
+			Path rootConfigPath = rootConfigDirectory.toPath();
+			Files.createDirectories(rootConfigPath);
+			
+			for (String defaultConfigFile : defaultConfigFiles) {
+				byte[] templateData = IOUtils.resourceToByteArray("/config-template/"+defaultConfigFile);
+				FileUtils.writeByteArrayToFile(rootConfigPath.resolve(defaultConfigFile).toFile(), templateData);
+			}
+		}
+		
 		entryTypeManager = new EntryTypeManager(rootConfigDirectory);
 		tagGroupManager = new TagGroupManager(rootConfigDirectory);
 	}
