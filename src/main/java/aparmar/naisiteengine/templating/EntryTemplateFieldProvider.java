5f9f41db4ee60357a8b726454125b94b332ad19b
aparmar2000 <aparmar2000@gmail.com>
2023-12-19 17:16:28 -0700

    Added truncation capability to entry field template

--- src/main/java/aparmar/naisiteengine/templating/EntryTemplateFieldProvider.java
+++ src/main/java/aparmar/naisiteengine/templating/EntryTemplateFieldProvider.java
@@ -1,6 +1,7 @@
 package aparmar.naisiteengine.templating;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 
@@ -13,6 +14,11 @@ public class EntryTemplateFieldProvider extends AbstractEntryTemplateProvider {
 	public static final String ENTRY_FIELD_SPECIAL_KEY = "entry-field";
 	
 	private static final String ENTRY_FIELD_NAME_PARAM_KEY = "name";
+	private static final String FIELD_LENGTH_LIMIT_PARAM_KEY = "max-len";
+	
+	private static final String FIELD_TRUNC_METHOC_PARAM_KEY = "trunc-method";
+	private static final String TRUNC_METHOD_CHARACTER = "CHAR";
+	private static final String TRUNC_METHOD_WORD = "WORD";
 	
 	private static final Map<String, Function<EntryData,String>> SPECIAL_FIELDS_MAP = ImmutableMap.of(
 			"id", e->Integer.toString(e.getId()),
@@ -32,12 +38,46 @@ public class EntryTemplateFieldProvider extends AbstractEntryTemplateProvider {
 			return templateName+" parse error: missing 'name' parameter";
 		}
 		
+		int maxFieldLength = Optional.ofNullable(templateParams.get(FIELD_LENGTH_LIMIT_PARAM_KEY))
+				.map(Integer::parseInt)
+				.orElse(Integer.MAX_VALUE);
+		String truncStrategy = Optional.ofNullable(templateParams.get(FIELD_TRUNC_METHOC_PARAM_KEY))
+				.map(String::toUpperCase)
+				.orElse(TRUNC_METHOD_WORD);
+		if (!truncStrategy.equals(TRUNC_METHOD_CHARACTER) && !truncStrategy.equals(TRUNC_METHOD_WORD)) {
+			return templateName+" parse error: unknown trunc-method '"+truncStrategy+"'";
+		}
+		
 		if (entryData.getFieldMap().containsKey(entryFieldName)) {
-			return entryData.getFieldMap().get(entryFieldName);
+			return truncateFieldValue(entryData.getFieldMap().get(entryFieldName), maxFieldLength, truncStrategy);
 		}
 		if (SPECIAL_FIELDS_MAP.containsKey(entryFieldName)) {
-			return SPECIAL_FIELDS_MAP.get(entryFieldName).apply(entryData);
+			return truncateFieldValue(SPECIAL_FIELDS_MAP.get(entryFieldName).apply(entryData), maxFieldLength, truncStrategy);
 		}
 		return templateName+" parse error: '"+entryFieldName+"' is not a valid field name";
+	}
+	
+	private String truncateFieldValue(String value, int maxLength, String truncStrategy) {
+		if (value.length()<=maxLength) {
+			return value;
+		}
+		
+		switch (truncStrategy) {
+		case TRUNC_METHOD_CHARACTER:
+			return value.substring(0, maxLength-3)+"...";
+		default:
+		case TRUNC_METHOD_WORD:
+			String result = "";
+			for (String section : value.split(" ")) {
+				String nextResult = result+" "+section;
+				if (nextResult.length()>maxLength-3) {
+					return result+"...";
+				}
+				result = nextResult;
+			}
+			break;
+		}
+		
+		return "err: String truncation error";
 	}
 }
